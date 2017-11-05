02348490cd308588dc57c7df6c6abdfe86d51d2f
zeokav <sakettsingh@outlook.com>
2017-11-05 11:32:23 +0530

    Changed network

---
+++ playwithagent.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import tensorflow as tf
+import cv2
+from game import wrapped_flappy_bird as game
+import random
+import numpy as np
+from collections import deque
+
